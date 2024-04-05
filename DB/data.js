205730e618a26ce5c3a26eefb2c3ccfe7a695a57
Deveshwar01 <86057587+Deveshwar01@users.noreply.github.com>
2024-04-05 11:41:01 +0530

    Update data.js

--- DB/data.js
+++ DB/data.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = () => {
     mongoose
-        .connect('mongodb://localhost:27017', {
+        .connect('mongodb+srv://deveshwork01:ljGl29Thg45UVPak@cluster0.ziffjlr.mongodb.net/myDb', {
             dbName: "E-COMMERCE",
         })
         .then((c) => console.log(`Database Connected`))
